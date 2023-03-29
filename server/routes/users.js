b3f61cf8dd740f2b52cb181f427e646f445eaa04
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-29 22:51:19 +0200

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,4 +4,26 @@ import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
+//Adding the new user From client
+router.post("/register", async(req, res) => {
+    //We are getting the username that sent from the FrontEnd
+    const {username , password} = req.body;
+
+    //Find the user if Exist which is sent from the FrontEnd
+    const user = await UserModel.findOne({username});
+
+    //If user is Available which data come from FrontEnd and return
+    if(user) {
+        return res.json({message : "User already exist!"});
+    }
+    
+    //make the password unique password which is generated like hash password 
+    const hassPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({username, password : hassPassword});
+    await newUser.save()
+
+    res.json({message : "User Registered"});
+});
+
 export {router as userRouter};
