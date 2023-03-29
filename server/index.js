9e188a322ce2af27a869186e6eebc559f7af1a20
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-03-29 21:12:26 +0200

    Mern-Recipe

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import { userRouter } from './routes/users.js';
+
+//To generate a version of our API
+const app = express();
+
+//It is Always convert data into json which is come from the FrotEnd with every sigle request we made from FrontEnd
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+
+
+//MondoDb connection
+mongoose.connect(
+    "mongodb+srv://neelendrareddy123:neelu123@recipes.d1wd1dk.mongodb.net/recipes?retryWrites=true&w=majority",
+    {
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
+    }
+)
+
+
+app.listen(3001, () => {
+    console.log("Server Started");
+})
