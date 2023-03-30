aa27f606cf2582db34ea95819887705a8b19d4f0
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-30 19:52:13 +0200

    Update navbar.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,12 +3,20 @@ import { Link, useNavigate } from "react-router-dom"
 import {useCoolies} from 'react-cookie';
 
 export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+    const logOut = () => {
+        setCookies("access_token", "");
+        window.localStorage.removeItem("userId");
+        navigate("/auth");
+    }
     
     return (
         <div className="navbar">
             <Link to="/">Home</Link>
             <Link to="/create-recipe">Create Recipe</Link>
             <Link to="/saved-recipes">Saved Recipes</Link>
+            
             <Link to="/auth">Login/Register</Link>
         </div>
     )
