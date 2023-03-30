6914ace85f698dd423780ccc0471d8b6cc9b0b26
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-30 22:46:49 +0200

    Update recipes.js

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -23,5 +23,37 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.put("/", async (req, res) => {
+    try {
+        const recipe = await RecipeModel.findById(req.body.recipeId);
+        const user = await UserModel.findById(req.body.userId);
+        user.savedRecipe.push(recipe);
+        await user.save();
+        res.json({savedRecipe : user.savedRecipe});
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.get("/savedRecipes/ids", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userId);
+        res.json({savedRecipe: user?.savedRecipe})
+    } catch (err) {
+        res.json(err);
+    }
+})
+
+router.get("/savedRecipes", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userId);
+        const savedRecipes = await RecipeModel.find({
+            _id : {$in : user.savedRecipe},
+        });
+        res.json({savedRecipes : user?.savedRecipe});
+    } catch(err) {
+        res.json(err);
+    }
+});
 
 export {router as recipeRouter};
