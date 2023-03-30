00fb63409fb0b694cfe02179144f3199e6999e9c
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-30 22:08:47 +0200

    Update recipes.js

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { RecipeModel } from '../models/Recipes';
+import { RecipeModel } from '../models/Recipes.js';
 
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const response = await RecipeModel.find({});
+        res.json(response);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+
 
 export {router as recipeRouter};
