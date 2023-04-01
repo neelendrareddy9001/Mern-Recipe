87c8178b3d061f8c69e067081a4c3d41417db6f7
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-01 12:24:31 +0200

    Update createRecipe.js

--- client/src/pages/createRecipe.js
+++ client/src/pages/createRecipe.js
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
 export const CreateRecipe = () => {
-    return <div>Create Recipe</div>
+    const [recipe, setRecipe] = useState({
+        name : "",
+        ingredients : [],
+        instructions  : "",
+        imageUrl : "",
+        cookingTime : 0,
+        userOwner : 0,
+    });
+    const handleChange = (event) =>{
+        const {name, value} = event.target;
+        setRecipe({...recipe, [name] : value});
+    }
+    
+    return (
+        <div className="create-recipe">
+            <h2>Create Recipe</h2>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" onChange={handleChange}></input>
+                <lable htmlFor="ingredients">Ingredients</lable>
+                <label htmlFor="instructions">Instructions</label>
+                <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+                <label htmlFor="imageUrl">Image Url</label>
+                <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}></input>
+                <label htmlFor="cookingTime">Cookingn Time (minutes)</label>
+                <input type="submit" id="cookingTime" name="cookingTime" onChange={handleChange}></input>
+            </form>
+        </div>
+    )
 }
