4a5265a00cd458bc50ff072f66d9be95bc8dfe29
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-08-31 16:43:31 +0200

    updated recipes and user Routes pages

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,49 +1,54 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
+import { UserModel } from "../models/Users.js";
 
-//Adding the new user From client
-router.post("/register", async(req, res) => {
-    //We are getting the username that sent from the FrontEnd
-    const {username , password} = req.body;
-
-    //Find the user if Exist which is sent from the FrontEnd
-    const user = await UserModel.findOne({username});
-
-    //If user is Available which data come from FrontEnd and return
-    if(user) {
-        return res.json({message : "User already exist!"});
-    }
-    
-    //make the password unique password which is generated like hash password 
-    const hassPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new UserModel({username, password : hassPassword});
-    await newUser.save()
-
-    res.json({message : "User Registered"});
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+  if (user) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+  res.json({ message: "User registered successfully" });
 });
 
-router.post("/login", async(req, res) => {
-    //sent from Client
-    const {username, password} = req.body;
-    const user = await UserModel.findOne({username});
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    //if not found the tuser from the Client
-    if(!user) {
-        return res.json({message : "User Doesn't Exist"});
-    }
+  const user = await UserModel.findOne({ username });
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if(!isPasswordValid) {
-        return res.json({message : "Username or Password is InCorrect"});
-    }
-
-    const token = jwt.sign({id : user._id}, "secret");
-    res.json({token, userId : user._id});
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "Username or password is incorrect" });
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res
+      .status(400)
+      .json({ message: "Username or password is incorrect" });
+  }
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
 });
 
-export {router as userRouter};
+export { router as userRouter };
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, "secret", (err) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
